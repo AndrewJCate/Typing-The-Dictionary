111fc7cd37c41d2640300b41075f92f040bef9f3
OnG2FNhu7!!8g0nZ0eKFI@cX <andrewjcate@gmail.com>
2024-04-15 13:47:02 -0700

    Refactors how stats are calculated and saved.

--- src/main/java/com/cate/typingthedictionary/PlayerData.java
+++ src/main/java/com/cate/typingthedictionary/PlayerData.java
@@ -1,31 +1,48 @@
 package com.cate.typingthedictionary;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class PlayerData {
 
     private static final PlayerData INSTANCE = new PlayerData();
+    private static final int AVERAGE_WORD_LENGTH = 5;
 
+    private long globalElapsedSeconds  = 0;
+    private int  globalErrors          = 0;
+    private long globalKeysPressed     = 0;
+    private int  globalWordsTyped      = 0;
 
-    private int globalErrors = 0;
-    private int globalTotalWordsTyped = 0;
-    private int globalWPM = 0;
+    private long sessionElapsedSeconds = 0;
+    private int  sessionErrors         = 0;
+    private long sessionKeysPressed    = 0;
+    private int  sessionWordsTyped     = 0;
 
-    private int sessionErrors = 0;
-    private int sessionTotalWordsTyped = 0;
-    private int sessionWPM = 0;
-
-    private int entryErrors = 0;
-    private int entryWordsTyped = 0;
+    private int  entryErrors = 0;
 
     private String userName = null;
 
-    private PlayerData() {
-    }
+
+    /* Constructor */
+
+    private PlayerData() {}
 
     public static PlayerData getInstance() {
         return INSTANCE;
     }
 
-    public int getGlobalErrors() {
+
+    /* Default field getter/setters */
+
+    public long getGlobalElapsedSeconds() {
+        return globalElapsedSeconds;
+    }
+
+    public void setGlobalElapsedSeconds(long globalElapsedSeconds) {
+        this.globalElapsedSeconds = globalElapsedSeconds;
+    }
+
+    public int  getGlobalErrors() {
         return globalErrors;
     }
 
@@ -33,99 +50,135 @@ public class PlayerData {
         this.globalErrors = globalErrors;
     }
 
-    public int getGlobalTotalWordsTyped() {
-        return globalTotalWordsTyped;
+    public long getGlobalKeysPressed() {
+        return globalKeysPressed;
     }
 
-    public void setGlobalTotalWordsTyped(int globalTotalWordsTyped) {
-        this.globalTotalWordsTyped = globalTotalWordsTyped;
+    public void setGlobalKeysPressed(long globalKeysPressed) {
+        this.globalKeysPressed = globalKeysPressed;
     }
 
-    public int getGlobalWPM() {
-        return globalWPM;
+    public int  getGlobalWordsTyped() {
+        return globalWordsTyped;
     }
 
-    public void setGlobalWPM(int globalWPM) {
-        this.globalWPM = globalWPM;
-    }
-
-    public int getSessionErrors() {
-        return sessionErrors;
-    }
-
-    public void setSessionErrors(int sessionErrors) {
-        this.sessionErrors = sessionErrors;
-    }
-
-    public int getSessionTotalWordsTyped() {
-        return sessionTotalWordsTyped;
-    }
-
-    public void setSessionTotalWordsTyped(int sessionTotalWordsTyped) {
-        this.sessionTotalWordsTyped = sessionTotalWordsTyped;
-    }
-
-    public int getSessionWPM() {
-        return sessionWPM;
-    }
-
-    public void setSessionWPM(int sessionWPM) {
-        this.sessionWPM = sessionWPM;
-    }
-
-    public int getEntryErrors() {
-        return entryErrors;
-    }
-
-    public void setEntryErrors(int entryErrors) {
-        this.entryErrors = entryErrors;
-    }
-
-    public int getEntryWordsTyped() {
-        return entryWordsTyped;
-    }
-
-    public void setEntryWordsTyped(int entryWordsTyped) {
-        this.entryWordsTyped = entryWordsTyped;
+    public void setGlobalWordsTyped(int globalWordsTyped) {
+        this.globalWordsTyped = globalWordsTyped;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
+    public void   setUserName(String userName) {
         this.userName = userName;
     }
 
-    public void saveEntryData() {
-        sessionTotalWordsTyped += entryWordsTyped;
-        sessionErrors += entryErrors;
-    }
 
-    public void resetEntryData() {
-        entryErrors = 0;
-        entryWordsTyped = 0;
-    }
-
-    public int calculateSessionAccuracy() {
-        return calculateAccuracy(sessionErrors + entryErrors, sessionTotalWordsTyped + entryWordsTyped);
-    }
-
-    public void saveSessionData() {
-        globalTotalWordsTyped += sessionTotalWordsTyped;
-        globalErrors += sessionErrors;
-    }
+    /* Public methods */
 
     public void incrementEntryErrorCount() {
         entryErrors++;
     }
 
+    public int  getGlobalAccuracy() {
+        return calculateAccuracy(globalErrors, globalWordsTyped);
+    }
+
+    public int  getGlobalWPM() {
+        return calculateWPM(globalKeysPressed, globalElapsedSeconds, getGlobalAccuracy());
+    }
+
+    public int  getSessionAccuracyWithEntry(String inputText) {
+
+        int entryWordsTyped        = countTypedWords(inputText);
+        int totalSessionWordsTyped = entryWordsTyped + sessionWordsTyped;
+        int totalSessionErrors     = entryErrors + sessionErrors;
+
+        return calculateAccuracy(totalSessionErrors, totalSessionWordsTyped);
+    }
+
+    public int  getSessionWordsTyped(String inputText) {
+
+        int wordsTyped = countTypedWords(inputText);
+
+        return wordsTyped + sessionWordsTyped;
+    }
+
+    public int  getSessionWPM(String inputText, Instant start, Instant stop) {
+
+        if (start == null || stop == null) {
+            return calculateWPM(sessionKeysPressed, sessionElapsedSeconds, getSessionAccuracyWithoutEntry());
+        }
+
+        long totalSessionKeysPressed    = inputText.length() + sessionKeysPressed;
+        long entryElapsedSeconds        = Duration.between(start, stop).toSeconds();
+        long totalSessionElapsedSeconds = entryElapsedSeconds + sessionElapsedSeconds;
+
+        return calculateWPM(totalSessionKeysPressed, totalSessionElapsedSeconds, getSessionAccuracyWithEntry(inputText));
+    }
+
+    public void resetEntryData() {
+        entryErrors = 0;
+    }
+
+    public void saveAllData(String inputText, Instant start, Instant stop) {
+        saveEntryDataToSessionData(inputText, start, stop);
+        saveSessionDataToGlobalData();
+    }
+
+
+    /* Private methods */
+
     private int calculateAccuracy(int numErrors, int totalWords) {
 
-        if (totalWords == 0) return 0;
+        if (numErrors == 0) return 100;
 
         return (int) (100 - (numErrors / (double) totalWords) * 100);
 
+    }
+
+    private int calculateWPM(long keysPressed, long elapsedSeconds, int accuracy) {
+
+        long numWords = keysPressed / AVERAGE_WORD_LENGTH;
+
+        if (elapsedSeconds == 0) {
+            elapsedSeconds = 1;
+        }
+
+        double wpm = numWords / (elapsedSeconds / 60.0); // 60 seconds in one minute
+
+        return (int)(wpm * (accuracy / 100.0));
+    }
+
+    private int countTypedWords(String inputText) {
+
+        if (inputText.isEmpty()) return 0;
+
+        return inputText.split("\\s+").length;
+    }
+
+    private int getSessionAccuracyWithoutEntry() {
+        return calculateAccuracy(sessionErrors, sessionWordsTyped);
+    }
+
+    private void saveEntryDataToSessionData(String inputText, Instant start, Instant stop) {
+
+        int entryWordsTyped      = countTypedWords(inputText);
+        long entryElapsedSeconds = Duration.between(start, stop).toSeconds();
+
+        sessionErrors         += entryErrors;
+        sessionElapsedSeconds += entryElapsedSeconds;
+        sessionKeysPressed    += inputText.length();
+        sessionWordsTyped     += entryWordsTyped;
+    }
+
+    private void saveSessionDataToGlobalData() {
+
+        globalErrors         += sessionErrors;
+        globalElapsedSeconds += sessionElapsedSeconds;
+        globalKeysPressed    += sessionKeysPressed;
+        globalWordsTyped     += sessionWordsTyped;
     }
 
 }
